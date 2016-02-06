8bcf67f98761c22d1d928685c21ffa6038679cbc
Dean Silfen <dean.silfen@gmail.com>
2016-02-06 11:31:54 -0500

    Add auth for slack

--- bot.rb
+++ bot.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'json'
 
 class QueueBot < Sinatra::Base
+  before '/*' do
+    data = JSON.parse(request.body.read)
+    if data['token'] != ENV['SLACK_TOKEN']
+      halt 401, 'Not Authorized'
+    end
+  end
+
   post '/api/v1/queue/?' do
     data = JSON.parse(request.body.read)
     text = data['text']
